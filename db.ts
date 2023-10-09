d740f66077f9288592a521c2bf1315524c6ec1e7
Dan Woodson <woodson.dan@gmail.com>
2023-10-09 12:54:10 -0600

    Adding a ci fallback for env vars

--- db.ts
+++ db.ts
@@ -2,8 +2,8 @@ import { load } from 'https://deno.land/std@0.202.0/dotenv/mod.ts'
 import Storage from 'https://denopkg.com/ultraxlight/storage@0.1.3/implementations/supabase.ts'
 
 const env = await load({ envPath: './.env.develop' })
-const url = env['SUPABASE_URL']
-const publicAnonKey = env['SUPABASE_PUBLIC_ANON_KEY']
+const url = env['SUPABASE_URL'] || Deno.env.get('SUPABASE_URL')
+const publicAnonKey = env['SUPABASE_PUBLIC_ANON_KEY'] || Deno.env.get('SUPABASE_PUBLIC_ANON_KEY')
 
 await Storage.init({url, publicAnonKey})
 
