fb868fe1c20a45d8928dd27ff7ce2df1fb666815
Dan Woodson <woodson.dan@gmail.com>
2023-10-31 16:34:37 -0600

    feat(lists): :sparkles: List names are now links to that list

--- routes/lists/index.tsx
+++ routes/lists/index.tsx
@@ -1,8 +1,6 @@
 import { Handlers } from "$fresh/server.ts";
 import List from "https://denopkg.com/ultraxlight/lists@0.2.1/src/lists/list/mod.ts";
 import ListItem from "https://denopkg.com/ultraxlight/lists@0.2.1/src/list-items/list-item/mod.ts";
-import DeleteListItem from "../../islands/DeleteListItem.tsx";
-import EditListItem from "../../islands/EditListItem.tsx";
 import Storage from "./db.ts";
 import ListItemStorage from "../list-items/db.ts";
 
@@ -58,9 +56,7 @@ export default async function Lists() {
       <ul>
         {items.map((item) => (
           <li>
-            {typeof item.title === "string" ? item.title : ""}
-            <EditListItem id={item.id} value={item.title} />
-            <DeleteListItem id={item.id} />
+            <a href={`/lists/${item.id}`}>{item.title}</a>
           </li>
         ))}
       </ul>
